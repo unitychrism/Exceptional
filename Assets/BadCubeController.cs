ad8b5e9aeb29a65457c717c1f36c4aded7c2a110
Chris Mahoney <chrism@unity3d.com>
2016-01-25 13:37:58 -0600

    Maybe I fixed things

--- Assets/BadCubeController.cs
+++ Assets/BadCubeController.cs
@@ -6,6 +6,9 @@ public class BadCubeController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		m_DoesntExist = "text string";
+
+		m_ThisAlsoDoesntExist = "something else";
+		m_BadVariable = "bad";
 	}
 	
 	// Update is called once per frame
