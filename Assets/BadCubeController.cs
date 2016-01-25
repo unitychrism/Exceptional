ea5a9bd0be7ea25eed6bbf130ccd4e18b23d8b7e
Chris Mahoney <chrism@unity3d.com>
2016-01-25 12:43:06 -0600

    I guess the Assets folder is necessary, so I'll add it

---
+++ Assets/BadCubeController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BadCubeController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		m_DoesntExist = "text string";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
