d391208b5baee7df18828ee9d11d1a86e74535f1
Chris Mahoney <chrism@unity3d.com>
2016-01-25 14:06:01 -0600

    Adjust main camera/break everything all over again and make it worse

--- Assets/BadCubeController.cs
+++ Assets/BadCubeController.cs
@@ -3,14 +3,14 @@ using System.Collections;
 
 public class BadCubeController : MonoBehaviour {
 
-	public string m_DoesntExist, m_ThisAlsoDoesntExist, m_BadVariable;
-
 	// Use this for initialization
 	void Start () {
 		m_DoesntExist = "text string";
 
 		m_ThisAlsoDoesntExist = "something else";
 		m_BadVariable = "bad";
+
+		m_MoreBadThings = "really bad";
 	}
 	
 	// Update is called once per frame
